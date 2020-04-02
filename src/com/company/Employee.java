2e1cf3befd9b4ddb7d336cad357bbd85b1718f0d
Daniil_E <danny190100@gmail.com>
2020-04-02 20:30:37 +0300

    ошибка

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -5,12 +5,38 @@ public class Employee {
     private int depar;
     private int sal;
     public static int j;
+    public static int dp1;
+    public static int dp2;
 
     public Employee(String name, int sal, int depar) {
         this.name = name;
         this.sal = sal;
         this.depar = depar;
         j++;
+        if (this.depar == 1) {
+            dp1++;
+        } else {
+            dp2++;
+        }
     }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSal() {
+        return this.sal;
+    }
+
+    public void setSal(int sal) { this.sal = sal; }
+
+    public int getDepar() {
+        return this.depar;
+    }
+
+    public void setDepar(int depar) { this.depar = depar; }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,32 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
+
         Employee emp1 = new Employee("sdfsd", 8, 1);
         Employee emp2 = new Employee("iasf", 4, 2);
         Employee emp3 = new Employee("kdsfdf", 6, 1);
         Employee emp4 = new Employee("ddfsd" , 4, 1);
         Employee emp5 = new Employee("rdfsd", 9, 2);
 
+        String depar1Arr[] = new String[Employee.dp1];
+        String depar2Arr[] = new String[Employee.dp2];
         int salArr[] = new  int[Employee.j];
 
-        for (int i=0; i<Employee.j; i++){
-
-        }
+        if (emp1.getDepar()==1){ depar1Arr[1] = emp1.getName();} else {depar2Arr[1] = emp1.getName();}
+        salArr[1] = emp1.getSal();
+        if (emp2.getDepar()==1){ depar1Arr[2] = emp2.getName();} else {depar2Arr[2] = emp2.getName();}
+        salArr[2] = emp1.getSal();
+        if (emp1.getDepar()==1){ depar1Arr[5] = emp5.getName();} else {depar2Arr[5] = emp5.getName();}
+        salArr[5] = emp1.getSal();
+        if (emp1.getDepar()==1){ depar1Arr[4] = emp4.getName();} else {depar2Arr[4] = emp4.getName();}
+        salArr[4] = emp1.getSal();
+        if (emp1.getDepar()==1){ depar1Arr[3] = emp3.getName();} else {depar2Arr[3] = emp3.getName();}
+        salArr[3] = emp1.getSal();
 
         Scanner scanner = new Scanner(System.in);
         int a;
@@ -23,8 +34,7 @@ public class Main {
             System.out.println("Enter department");
             a=scanner.nextInt();
         }while (a!=1 || a!=2);
-        switch (a){
-            case(a==1)
-        }
+        if (a==1){System.out.println(Arrays.toString(depar1Arr));}
+        else{System.out.println(Arrays.toString(depar2Arr));}
     }
 }
